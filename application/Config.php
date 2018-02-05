7068c347ad4c787534538e289e634fba5954c122
Segundo <segundogaldames@gmail.com>
2018-02-05 14:09:08 -0300

    Configuracion de base de datos en Heroku

--- application/Config.php
+++ application/Config.php
@@ -7,8 +7,8 @@ define('DEFAULT_LAYOUT', 'default');
 define('SESSION_TIME', 10);
 define('HASH_KEY', '549fec1d3212');
 
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '1234');
-define('DB_NAME', 'qepd');
+define('DB_HOST', 'us-cdbr-iron-east-05.cleardb.net');
+define('DB_USER', 'b7c3db82bbf6d4');
+define('DB_PASS', '96f14ed9');
+define('DB_NAME', 'heroku_4420a371c48fb85');
 define('DB_CHAR', 'utf8');
